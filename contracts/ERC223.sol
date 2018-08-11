2e91024b31eaf4a147bad011e4f22defe163f07d
abasalilov <abasalilov@gmail.com>
2018-08-11 14:35:08 -0700

    working

--- contracts/ERC223.sol
+++ contracts/ERC223.sol
@@ -264,19 +264,6 @@ contract ERC223 is ERC20, SafeMath {
         return true;
     }
 
-    // function that is called when transaction target is a contract
-    function transferToContract(address _to, uint _value, bytes _data) private returns (bool success) {
-        if (balanceOf(msg.sender) < _value) {
-            revert();
-        }
-        balances[msg.sender] = safeSub(balanceOf(msg.sender), _value);
-        balances[_to] = safeAdd(balanceOf(_to), _value);
-        ContractReceiver receiver = ContractReceiver(_to);
-        receiver.tokenFallback(msg.sender, _value, _data);
-        emit Transfer(msg.sender, _to, _value, _data);
-        return true;
-    }
-
     // Get balance of the address provided
     function balanceOf(address _owner) constant public returns (uint256 balance) {
         return balances[_owner];
--- test/ERC223Test.sol
+++ test/ERC223Test.sol
@@ -4,32 +4,31 @@ import "truffle/Assert.sol";
 import "../contracts/ERC223.sol";
 import "truffle/DeployedAddresses.sol";
 import "./helpers/ContractReceiverOverrider.sol";
-import "./helpers/ContractReceiver.sol";
+// import "./helpers/ContractReceiver.sol";
 
 // Contract that isn't working with ERC223 tokens
-contract ContractNonReceiver {
-}
+contract ContractNonReceiver {}
 
 // Proxy contract for testing throws
-// contract ThrowProxy {
-//   address public target;
-//   bytes data;
-//   constructor(address _target) {
-//     target = _target;
-//   }
+contract ThrowProxy {
+  address public target;
+  bytes data;
+  constructor(address _target) {
+    target = _target;
+  }
 
-//   //prime the data using the fallback function.
-//   function() {
-//     data = msg.data;
-//   }
+  //prime the data using the fallback function.
+  function() {
+    data = msg.data;
+  }
 
-//   function tokenFallback(address _from, uint _value, bytes _data) {
-//   }
+  function tokenFallback(address _from, uint _value, bytes _data) {
+  }
 
-//   function execute() returns (bool) {
-//     return target.call(data);
-//   }
-// }
+  function execute() returns (bool) {
+    return target.call(data);
+  }
+}
 
 
 contract ERC223Test {
@@ -38,51 +37,72 @@ contract ERC223Test {
   string tokenSymbol = "LED";
   event Show (string addy);
 
-  ContractReceiver receiver = new ContractReceiver();
-  uint expected = 10000000000000000;
-  // bytes memory empty;
+  function testSettingAnOwnerDuringCreation() public {
+      ERC223 erc223 = new ERC223();
+      Assert.equal(erc223.balanceOf(address(this)), 10000000000000000000, "An owner is different than a deployer");
+    }
 
-function testSettingAnOwnerDuringCreation() public {
-    ERC223 erc223 = new ERC223();
-    Assert.equal(erc223.balanceOf(tx.origin), 10000000000000000000, "An owner is different than a deployer");
-  }
+  function testSettingAnOwnerOfDeployedContract() public {
+      // setting an owner of deployed contract, instead of new
+      ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
+      Assert.equal(erc223.owner(), msg.sender, "An owner is different than a deployer");
+    }
 
-// function testSettingAnOwnerOfDeployedContract() public {
-//     // setting an owner of deployed contract, instead of new
-//     ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
-//     Assert.equal(erc223.owner(), msg.sender, "An owner is different than a deployer");
-//   }
-
-// function testSettingATokenNameOfContract() public {
-//     // setting an owner of deployed contract, instead of new
-//     ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
-//     Assert.equal(erc223.name(), tokenName, "The token name is different than expected");
-//   }
+  function testSettingATokenNameOfContract() public {
+      // setting an owner of deployed contract, instead of new
+      ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
+      Assert.equal(erc223.name(), tokenName, "The token name is different than expected");
+    }
 
 
-// function testSettingASymbolOfContract() public {
-//     // setting an owner of deployed contract, instead of new
-//     ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
-//     Assert.equal(erc223.symbol(), tokenSymbol, "The token symbol is different than expected");
-//   }
+  function testSettingASymbolOfContract() public {
+      // setting an owner of deployed contract, instead of new
+      ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
+      Assert.equal(erc223.symbol(), tokenSymbol, "The token symbol is different than expected");
+    }
 
-// function testSettingATotalSupply() public {
-//     // setting an owner of deployed contract, instead of new
-//     ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
-//     Assert.equal(erc223.totalSupply(), 10000000000000000000, "The token total supply is different than 100000000000 * 10**8;");
-//   }
+  function testSettingATotalSupply() public {
+      uint expected = 10000000000000000000;
+      // setting an owner of deployed contract, instead of new
+      ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
+      Assert.equal(erc223.totalSupply(), expected, "The token total supply is different than 100000000000 * 10**8;");
+    }
 
-// function testInitialTransferToOwner() public {
-//     ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
-//     Assert.equal(erc223.balanceOf(msg.sender), 10000000000000000000, "Initial token amount transfer to owner is different than 0");
-//   }
+  function testInitialTransferToOwner() public {
+      uint expected = 10000000000000000000;
+      ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
+      Assert.equal(erc223.balanceOf(msg.sender), expected, "Initial token amount transfer to owner is different than 0");
+    }
 
-function testInitialTransferToPurchaser() public {
-    // ERC223 erc223 = new ERC223();
-    ERC223 erc223 = ERC223(DeployedAddresses.ERC223());
-    emit Show("THE");
-    // erc223.transfer(msg.sender, 100)
-    // Assert.equal(erc223.balanceOf(msg.sender), 100, "Transfer of 1000 tokens to `this` address and balance not confirmed");
-  }
+  // function testTraspasingWithDataToInvalidContract() {
+  //     ERC223 token = new ERC223();
+  //     ContractNonReceiver nonReceiver = new ContractNonReceiver();
+
+  //     ThrowProxy throwProxy = new ThrowProxy(address(token));
+
+  //     uint expected = 10000000000000000000;
+  //     bytes memory empty;
+
+      // Assert.equal(token.balanceOf(address(this)), expected, "Owner should have 1000 LED Coin initially");
+      /* Proxy Contract will execute and catch the throw so it needs the balance */
+      // Assert.isTrue(token.transfer(address(throwProxy), 1000), 'Transfer to Proxy success');
+      // ERC223(address(throwProxy)).transfer(address(nonReceiver), 50, empty);
+      // bool r = throwProxy.execute.gas(2000)();
+      // Assert.isFalse(r, "Should be false, as it should throw");
+      // Assert.equal(token.balanceOf(address(throwProxy)), 100, "Owner should have 50 LED Coin initially");
+      // Assert.equal(token.balanceOf(address(nonReceiver)), 0, "Owner should have 50 LED Coin initially");
+    // }
+
+  // function testTraspasingWithoutDataToValidContract() {
+  //   ERC223 token = new ERC223();
+
+  //   ContractReceiver receiver = new ContractReceiver();
+  //   uint expected = 10000000000000000;
+
+  //   Assert.equal(token.balanceOf(address(this)), expected, "Owner should have 10000000000000000 MetaCoin initially");
+  //   Assert.isTrue(token.transfer(address(receiver), 5000000000000000), 'Transfer success');
+  //   Assert.equal(token.balanceOf(address(this)), 5000000000000000, "Owner should have 5000000000000000 MetaCoin initially");
+  //   Assert.equal(token.balanceOf(address(receiver)), 5000000000000000, "Owner should have 5000000000000000 MetaCoin initially");
+  // }
     
 }
